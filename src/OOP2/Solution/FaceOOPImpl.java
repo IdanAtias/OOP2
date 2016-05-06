354595465866fd5a42dc1cbf79ae25b1f84a8b14
IdanAtias <idan2k3@gmail.com>
2016-05-06 13:46:56 +0300

    Revert "conflict resolved"
    This reverts commit 2b5f09e0daa34bd779c4a778ca1ee68af5a08243.

--- src/OOP2/Solution/FaceOOPImpl.java
+++ src/OOP2/Solution/FaceOOPImpl.java
@@ -33,8 +33,8 @@ public class FaceOOPImpl implements FaceOOP {
 	/**
 	 * Constructor - receives no parameters and initializes the system.
 	 */
-
-	public FaceOOPImpl() { }
+	public FaceOOPImpl() { //checking pull request / sync??
+	}
 
 	@Override
 	private Person joinFaceOOP(Integer id, String name) throws PersonAlreadyInSystemException {
