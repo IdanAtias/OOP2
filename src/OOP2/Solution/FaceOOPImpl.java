2b5f09e0daa34bd779c4a778ca1ee68af5a08243
IdanAtias <idan2k3@gmail.com>
2016-05-06 13:44:38 +0300

    conflict resolved

--- src/OOP2/Solution/FaceOOPImpl.java
+++ src/OOP2/Solution/FaceOOPImpl.java
@@ -33,8 +33,8 @@ public class FaceOOPImpl implements FaceOOP {
 	/**
 	 * Constructor - receives no parameters and initializes the system.
 	 */
-	public FaceOOPImpl() { //checking pull request / sync??
-	}
+
+	public FaceOOPImpl() { }
 
 	@Override
 	private Person joinFaceOOP(Integer id, String name) throws PersonAlreadyInSystemException {
